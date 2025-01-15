a0f51d80a90d840222534f36abe38588d2f319b5
jessicajnx <jessica.jaunaux@gmail.com>
2025-01-15 10:06:59 +0100

    er

--- vue/FormulaireVue.php
+++ vue/FormulaireVue.php
@@ -27,18 +27,110 @@ require_once 'script/SoumissionFormulaire.php';
         <div class="question">
             <div id="3">
             <p>3. Si vous deviez vous taper avec un animal, lequel choisiriez-vous ?</p>
-            <label><input type="checkbox" name="3" value="Sabrina"> Toucan</label><br>
-            <label><input type="checkbox" name="3" value="Louis"> Cochon</label><br>
-            <label><input type="checkbox" name="3" value="Ana"> Kangourou</label><br>
+            <label><input type="checkbox" name="3" value="Toucan"> Toucan</label><br>
+            <label><input type="checkbox" name="3" value="Cochon"> Cochon</label><br>
+            <label><input type="checkbox" name="3" value="Kangourou"> Kangourou</label><br>
         </div>
         <!-- Question 4 -->
         <div class="question">
             <div id="4">
             <p>4. De quel pays voudriez-vous être président(e)</p>
-            <label><input type="checkbox" name="4" value="Sabrina"> France</label><br>
-            <label><input type="checkbox" name="4" value="Louis"> Italie</label><br>
-            <label><input type="checkbox" name="4" value="Ana"> Canada</label><br>
+            <label><input type="checkbox" name="4" value="France"> France</label><br>
+            <label><input type="checkbox" name="4" value="Italie"> Italie</label><br>
+            <label><input type="checkbox" name="4" value="Canada"> Canada</label><br>
         </div>
+        <!-- Question 5 -->
+        <div class="question">
+            <div id="5">
+            <p>5. Si vous viviez dans un monde de fiction, lequel choisiriez-vous ?</p>
+            <label><input type="checkbox" name="5" value="LOTR"> La terre du milieu</label><br>
+            <label><input type="checkbox" name="5" value="Borderland"> Alice in Borderland</label><br>
+            <label><input type="checkbox" name="5" value="EldenRing"> Elden Ring</label><br>
+        </div>
+        <!-- Question 6 -->
+        <div class="question">
+            <div id="6">
+            <p>6. Qui est le plus beau chauve ?</p>
+            <label><input type="checkbox" name="6" value="Prof"> Le prof</label><br>
+            <label><input type="checkbox" name="6" value="Zizou"> Zizou</label><br>
+            <label><input type="checkbox" name="6" value="Sins"> Johnny Sins</label><br>
+        </div>
+        <!-- Question 7 -->
+        <div class="question">
+            <div id="7">
+            <p>7. Si vous etiez un langage informatique, lequel seriez-vous ?</p>
+            <label><input type="checkbox" name="7" value="Php"> php parce que je suis un éléphant</label><br>
+            <label><input type="checkbox" name="7" value="Python"> Python parce que j'aime me mordre la queue</label><br>
+            <label><input type="checkbox" name="7" value="C"> C parce que je suis con</label><br>
+        </div>
+        <!-- Question 8 -->
+        <div class="question">
+            <div id="8">
+            <p>8. Quelle est votre technique de drague préférée ?</p>
+            <label><input type="checkbox" name="8" value="Peur"> j'ai peur des femmes</label><br>
+            <label><input type="checkbox" name="8" value="Boire"> je montre ma capacité à boire beaucoup</label><br>
+            <label><input type="checkbox" name="8" value="Cinema"> je l'amène au cinema</label><br>
+        </div>
+        <!-- Question 9 -->
+        <div class="question">
+            <div id="9">
+            <p>9. Quelle musique préferez-vous chanter ?</p>
+            <label><input type="checkbox" name="9" value="UnderPressure"> Under Pressure de Queen et David Bowie</label><br>
+            <label><input type="checkbox" name="9" value="LaPrincesse"> La Princesse de Café Allongé</label><br>
+            <label><input type="checkbox" name="9" value="OneMoreLight"> One more light de Linkin Park</label><br>
+        </div>
+        <!-- Question 10 -->
+        <div class="question">
+            <div id="10">
+            <p>10. Aimez-vous les femmes ou les hommes ?</p>
+            <label><input type="checkbox" name="10" value="hommes"> Les hommes</label><br>
+            <label><input type="checkbox" name="10" value="femmes"> Les femmes</label><br>
+            <label><input type="checkbox" name="10" value="deux"> Les deux</label><br>
+        </div>
+        <!-- Question 11 -->
+        <div class="question">
+            <div id="11">
+            <p>11. Si vous deviez manger le même repas jusqu\’à la fin de vos jours, qu'est-ce que ce serait ?</p>
+            <label><input type="checkbox" name="11" value="padthai"> Pad thaï</label><br>
+            <label><input type="checkbox" name="11" value="couscous"> Couscous</label><br>
+            <label><input type="checkbox" name="11" value="pizzas"> Pizzas</label><br>
+        </div>
+        <!-- Question 12 -->
+        <div class="question">
+            <div id="12">
+            <p>12. Si tu devais faire un cosplay au lit, lequel choisirais tu ?</p>
+            <label><input type="checkbox" name="12" value="Nazgul"> Roi sorcier d'angmar</label><br>
+            <label><input type="checkbox" name="12" value="pizza"> Pizza</label><br>
+            <label><input type="checkbox" name="12" value="Ken"> Ken</label><br>
+        </div>
+        <!-- Question 13 -->
+        <div class="question">
+            <div id="13">
+            <p>13. Quel serais votre super-pouvoir ?</p>
+            <label><input type="checkbox" name="13" value="Fruits"> Parler aux fruits</label><br>
+            <label><input type="checkbox" name="13" value="Alcool"> Transformer l'eau en alcool</label><br>
+            <label><input type="checkbox" name="13" value="Memoire"> Vous faire perdre la mémoire</label><br>
+        </div>
+        <!-- Question 14 -->
+        <div class="question">
+            <div id="14">
+            <p>14. Si vous étiez un mot, lequel seriez-vous ?</p>
+            <label><input type="checkbox" name="14" value="Gobelet"> Gobelet</label><br>
+            <label><input type="checkbox" name="14" value="Mot"> Mot</label><br>
+            <label><input type="checkbox" name="14" value="Tunnel"> Tunnel</label><br>
+        </div>
+        <!-- Question 15 -->
+        <div class="question">
+            <div id="15">
+            <p>15. Quelle est votre fleur préférée ?</p>
+            <label><input type="checkbox" name="15" value="Jacinthes"> Jacinthes</label><br>
+            <label><input type="checkbox" name="15" value="Edelweiss"> Edelweiss</label><br>
+            <label><input type="checkbox" name="15" value="Tulipes"> Tulipes</label><br>
+        </div>
+
+        <button type="button" onclick="afficherSelections()">Soumettre</button>
+    </form>
+
 
 
 
