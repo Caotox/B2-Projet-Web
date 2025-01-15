f9dbdcf2795687891c6c7c44ecc1b5a4161f619e
jessicajnx <jessica.jaunaux@gmail.com>
2025-01-15 11:48:12 +0100

    er

---
+++ vueAdmin/GestionFormulaires.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once 'Site_User/vue/FormulaireUser.php';
+
+?>
+
+<body>
+    <h1> Gestion des formulaires </h1>
+    <ul>
+        <li>
+            <a href="FormulaireUser.php">Test de compatibilité amoureuse</a>
+        </li>
+    </ul>
+</body>
+
+<?php 
+
+?>
