fa08391f79d935f4563360a838bd5b9b8087a306
Yanis <156795279+Mezreah@users.noreply.github.com>
2025-01-15 12:39:17 +0100

    Add files via upload
    => Ajout header footer pour User + CSS

---
+++ vueUser/headerUser.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset = "UTF-8">
+    <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
+    <meta name =  "description" content = "La partie header des pages admin">
+    <meta name = "author" content = "Meziane Yanis">
+    <title>Header user</title>
+    <link rel="stylesheet" href="./CSS/headerUser.css">
+</head>
+
+<header>
+    <section id="elem-header-user">
+        <article>
+            <a href="#">
+                <img src="#" alt="logo">
+            </a>
+        </article>
+
+        <article>
+            <div id="header-user-title">
+                    <h1>Titre</h1>
+                <br>
+                    <p>
+                        <em>
+                            "L'amour au premier clique existe"
+                        </em>
+                    </p>
+            </div>
+        </article>
+
+        <article>
+            <a href="./connexionUserVue.php">
+                <img src="#" alt ="bonhomme pour s'inscrire">
+            </a>
+        </article>
+    </section>
+</header>
+<body>
+    
