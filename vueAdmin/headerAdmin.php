1e08e1b0b1b42d49f1ca4309416b20e057804db7
Yanis <156795279+Mezreah@users.noreply.github.com>
2025-01-15 12:37:41 +0100

    Add files via upload
    => Ajout CSS pour le header et footer pour la page admin

---
+++ vueAdmin/headerAdmin.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset = "UTF-8">
+    <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
+    <meta name =  "description" content = "La partie header des pages admin">
+    <meta name = "author" content = "Meziane Yanis">
+    <title>Header admin</title>
+    <link rel="stylesheet" href="./CSS/headerAdmin.css">
+</head>
+
+<header>
+    <section id="elem-header-admin">
+        <article>
+            <a href="#">
+                <img src="#" alt="logo">
+            </a>
+        </article>
+
+        <article>
+            <div id="header-admin-title">
+                    <h1>Titre</h1>
+                <br>
+                    <p>
+                        <em>
+                            "L'amour au premier clique existe"
+                        </em>
+                    </p>
+            </div>
+        </article>
+
+        <article>
+            <a href="./connexionAdminVue.php">
+                <img src="#" alt ="bonhomme pour s'inscrire">
+            </a>
+        </article>
+    </section>
+</header>
+<body>
+    
