ea5b331dfff87d2ecb8295e5c86dd2f7c1efa013
Yanis <156795279+Mezreah@users.noreply.github.com>
2025-01-16 11:19:53 +0100

    Add files via upload

--- vueUser/accueil.php
+++ vueUser/accueil.php
@@ -2,14 +2,58 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
+    <meta name =  "description" content = "La partie header des pages admin">
+    <meta name = "author" content = "Meziane Yanis">
+    <link rel="stylesheet" href="./vueUser/CSS/accueil.css">
     <title>Page d'accueil</title>
 </head>
-<h1>Accueil de mon Site</h1>
-<nav>
-    <a href="?route=connexionUser">Connexion User</a>
-</nav>
-<nav>
-    <a href="?route=inscriptionUser">Inscription User</a>
-</nav>
-</body>
+    <body>
+        <main>
+            <section id = 'accueilContent'>
+                <article>
+                    <h1>
+                        <em>
+                            L'amour Ouf
+                        </em>
+                    </h1>
+                </article>
+
+                <article>
+                    <p>
+                         Découvrez la magie de l'amour avec notre test de compatibilité amoureuse unique !Plongez dans une expérience captivante qui vous révélera les secrets de votre harmonie avec les 3 challengers constituant ce groupe : Jessica / Antoine et Yanis. En quelques minutes seulement, explorez les facettes cachées de votre relation et percez les mystères de votre complicité.
+                         Notre test, conçu par des experts en psychologie relationnelle, analyse subtilement vos personnalités, vos valeurs et vos aspirations. Il vous guidera à travers une série de questions ludiques et perspicaces, conçues pour révéler la véritable essence de votre connexion.
+                    </p>
+                <br>
+                    <p>
+                        Que vous soyez en couple depuis des années ou au début d'une nouvelle histoire, ce test vous offrira des insights précieux pour renforcer votre lien et cultiver une relation épanouissante. Préparez-vous à être surpris, amusés, et peut-être même émus en découvrant à quel point vous êtes faits l'un pour l'autre !
+                    </p>
+                <br>
+                    <p>
+                         N'attendez plus pour percer les secrets de votre amour. Lancez-vous dans cette      aventure romantique et découvrez si vous êtes vraiment sur la même longueur d'onde. L'amour n'aura plus de secrets pour vous !
+                    </p>
+                <br>
+                    <p>
+                        Afin d'accéder au test, je t'invite à te connecter ou à t'inscrire si tu n'as pas encore crée de compte
+                    </p>
+                </article>
+
+        <section id="boxUser">
+            <article class = "link">
+                <h3>Pour te connecter</h3>
+                    <nav>
+                        <a href="?route=connexionUser">Connexion</a>
+                    </nav>
+            </article>
+
+            <article class = "link">
+                <h3>Pour t'inscrire</h3>
+                    <nav>
+                        <a href="?route=inscriptionUser">Inscription</a>
+                    </nav>
+            </article>
+            </section>
+        </section>
+        </main>
+    </body>
 </html>
