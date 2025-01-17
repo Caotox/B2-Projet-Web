e8b795adf15e1eb5fc465fcc220e9cdc6d6b0794
jessicajnx <jessica.jaunaux@gmail.com>
2025-01-17 10:52:32 +0100

    css Voirusersadmin

--- vueAdmin/voirUsers.php
+++ vueAdmin/voirUsers.php
@@ -14,6 +14,7 @@ if ($action === 'voirUsers') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/voirUsers.css">
     <title>Liste des utilisateurs</title>
 </head>
 <body>
