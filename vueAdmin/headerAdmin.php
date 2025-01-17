74dbf2e39039337625f9a4c6e1146e06642e4796
jessicajnx <jessica.jaunaux@gmail.com>
2025-01-17 14:07:33 +0100

    er

--- vueAdmin/headerAdmin.php
+++ vueAdmin/headerAdmin.php
@@ -4,7 +4,7 @@
     <meta charset = "UTF-8">
     <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
     <title>Header user</title>
-    <link rel="stylesheet" href="vueAdmin/CSS/headerAdmin.css">
+    <link rel="stylesheet" href="CSS/headerAdmin.css">
 </head>
 
 <header>
